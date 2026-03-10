c11de342f3b376288f0132cca0b8ffa93365070f
Muhammad-Hassan-Developer <hassanbscs1@gmail.com>
2026-03-11 01:09:55 +0500

    fix(sidebar):sidebar fix from the about page

--- pages/about.py
+++ pages/about.py
@@ -1,5 +1,11 @@
 import streamlit as st
 
+
+st.set_page_config(
+    page_title="AI Portfolio",
+    layout="wide",
+    initial_sidebar_state="collapsed"  # <---- hide sidebar
+)
 def show():
 
     st.title("About")
